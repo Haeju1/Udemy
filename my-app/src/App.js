7cc0743fa872eb5ca47d1f21b64ebfeb7dd48910
Haeju Jeong <haejujeo@usc.edu>
2019-09-30 23:29:10 -0700

    first project

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi, Im here</h1>
+      <h1>yo</h1>
+    </div>
+  );
+}
+
+export default App;
